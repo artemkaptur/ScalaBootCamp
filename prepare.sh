75ec14d9cd695fbe031698bbfc675a821176aa00
Artsiom Kaptur <artsiomkaptur@gmail.com>
2020-12-20 18:06:35 +0300

    Updated prepare.sh with tzdata #2

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 ln -snf /usr/share/zoneinfo/Europe/Minsk /etc/localtime && echo Europe/Minsk > /etc/timezone
+sudo apt-get install -y tzdata
 sudo apt-get update -y
 sudo apt-get install -y apt-utils
 #java
