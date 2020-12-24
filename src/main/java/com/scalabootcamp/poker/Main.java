625fdae5c3aa2fcb1fa76c2da63f0cb5952c6693
Artsiom Kaptur <artsiomkaptur@gmail.com>
2020-12-24 17:33:20 +0300

    Added error handling for invalid test cases

--- src/main/java/com/scalabootcamp/poker/Main.java
+++ src/main/java/com/scalabootcamp/poker/Main.java
@@ -34,18 +34,24 @@ public class Main {
     private static List<String> getSolutionsForEachHand(List<String> testCasesStrings) {
         List<TestCase> testCases = testCasesStrings.parallelStream()
                 .map(testCase -> {
-                    List<String> temp = Arrays.asList(testCase.split(" "));
-                    String gameType = temp.get(0);
-                    String cardsOnTable = temp.get(1);
-                    if (!gameType.equals(FIVE_CARD_DRAW)) {
-                        return TestCase.builder()
-                                .gameType(gameType)
-                                .cardsOnTable(cardsOnTable)
-                                .hands(temp.subList(2, temp.size())).build();
+                    List<String> testCasesSplittedBySpace = Arrays.asList(testCase.split(" "));
+                    String errorMessage = getErrorMessageForTestCase(testCasesSplittedBySpace);
+                    if (errorMessage.isEmpty()) {
+                        String gameType = testCasesSplittedBySpace.get(0);
+                        String cardsOnTable = testCasesSplittedBySpace.get(1);
+                        if (!gameType.equals(FIVE_CARD_DRAW)) {
+                            return TestCase.builder()
+                                    .gameType(gameType)
+                                    .cardsOnTable(cardsOnTable)
+                                    .hands(testCasesSplittedBySpace.subList(2, testCasesSplittedBySpace.size())).build();
+                        } else {
+                            return TestCase.builder()
+                                    .gameType(gameType)
+                                    .hands(testCasesSplittedBySpace.subList(1, testCasesSplittedBySpace.size())).build();
+                        }
                     } else {
                         return TestCase.builder()
-                                .gameType(gameType)
-                                .hands(temp.subList(1, temp.size())).build();
+                                .errorMessage(errorMessage).build();
                     }
                 })
                 .collect(toList());
@@ -53,18 +59,59 @@ public class Main {
     }
 
     private static String getSolution(TestCase testCase) {
-        String solution;
-        switch (testCase.getGameType()) {
-            case (TEXAS_HOLDEM):
-                solution = sortHandsForTexasHoldem(testCase);
-                break;
-            case (FIVE_CARD_DRAW):
-                solution = sortHandsForFiveCardDraw(testCase);
-                break;
-            default:
-                solution = String.format("Error: Your game type '%s' is not one of these [texas-holdem, five-card-draw]", testCase.getGameType());
-                break;
+        String solution = "";
+        if (testCase.getErrorMessage() == null) {
+            switch (testCase.getGameType()) {
+                case (TEXAS_HOLDEM):
+                    solution = sortHandsForTexasHoldem(testCase);
+                    break;
+                case (FIVE_CARD_DRAW):
+                    solution = sortHandsForFiveCardDraw(testCase);
+                    break;
+            }
+        } else {
+            solution = String.format("Error: %s", testCase.getErrorMessage());
         }
         return solution;
+    }
+
+    private static String getErrorMessageForTestCase(List<String> testCasesSplittedBySpace) {
+        if (testCasesSplittedBySpace.size() < 3)
+            return "Not enough data for test case, please enter <game_type>, <cards_on_the_table> (if it's texas holdem) and several <hand block>";
+
+        if (!testCasesSplittedBySpace.get(0).equals(TEXAS_HOLDEM) && !testCasesSplittedBySpace.get(0).equals(FIVE_CARD_DRAW))
+            return String.format("Your game type '%s' is not one of these [texas-holdem, five-card-draw]", testCasesSplittedBySpace.get(0));
+
+        if (testCasesSplittedBySpace.get(1).length() != 10 && testCasesSplittedBySpace.get(0).equals(TEXAS_HOLDEM))
+            return "Texas holdem <cards_on_the_table> does not consist from 5 cards";
+
+        if (!testCasesSplittedBySpace.get(1).matches("[TJQKAdhcs0-9]+") && testCasesSplittedBySpace.get(0).equals(TEXAS_HOLDEM))
+            return "Texas holdem <cards_on_the_table> does not consist from allowed symbols";
+
+        if (testCasesSplittedBySpace.get(0).equals(FIVE_CARD_DRAW) &&
+                testCasesSplittedBySpace.subList(1, testCasesSplittedBySpace.size()).stream()
+                        .filter(tc -> tc.length() == 10)
+                        .count() != testCasesSplittedBySpace.size() - 1)
+            return "One or more Five card draw <hand block> does not consist from 5 cards";
+
+        if (testCasesSplittedBySpace.get(0).equals(FIVE_CARD_DRAW) &&
+                testCasesSplittedBySpace.subList(1, testCasesSplittedBySpace.size()).stream()
+                        .filter(tc -> tc.matches("[TJQKAdhcs0-9]+"))
+                        .count() != testCasesSplittedBySpace.size() - 1)
+            return "One or more Five card draw <hand block> does not consist from allowed symbols";
+
+        if (testCasesSplittedBySpace.get(0).equals(TEXAS_HOLDEM) &&
+                testCasesSplittedBySpace.subList(2, testCasesSplittedBySpace.size()).stream()
+                        .filter(tc -> tc.length() == 4)
+                        .count() != testCasesSplittedBySpace.size() - 2)
+            return "One or more Texas holdem draw <hand block> does not consist from 2 cards";
+
+        if (testCasesSplittedBySpace.get(0).equals(TEXAS_HOLDEM) &&
+                testCasesSplittedBySpace.subList(2, testCasesSplittedBySpace.size()).stream()
+                        .filter(tc -> tc.matches("[TJQKAdhcs0-9]+"))
+                        .count() != testCasesSplittedBySpace.size() - 2)
+            return "One or more Texas holdem draw <hand block> does not consist from allowed symbols";
+
+        return "";
     }
 }
--- src/main/java/com/scalabootcamp/poker/model/TestCase.java
+++ src/main/java/com/scalabootcamp/poker/model/TestCase.java
@@ -13,4 +13,5 @@ public class TestCase {
     private final String gameType;
     private final String cardsOnTable;
     private final List<String> hands;
+    private final String errorMessage;
 }
